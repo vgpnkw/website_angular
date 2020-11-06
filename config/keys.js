79eb73815b0e22bdd67bb9c72308bcdf64aa5151
vgpnkw <gapankowwitalik@mail.ru>
2020-11-06 13:08:48 +0300

    Adding authorization with JWT token and adding registration

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://v_gpnkw:13Feb2009@cluster0.fn5xu.mongodb.net/Angular?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://v_gpnkw:13Feb2009@cluster0.fn5xu.mongodb.net/Angular?retryWrites=true&w=majority',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,69 @@
-module.exports.login = function(req,res) {
-    res.status(200).json({
-        login: "from controllers"
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../config/keys')
+const User = require("../models/User")
+
+module.exports.login = async function(req,res) {
+    // res.status(200).json({
+    //     login: {
+    //         email: req.body.email,
+    //         password: req.body.password
+    //     }
+    // })
+    const candidate = await User.findOne({
+        email: req.body.email
     })
+    if (candidate){ 
+        // Проверка пароля, пользвоатель существует
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult){
+            // Генерация токена
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 3600})
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                message: `Неверный пароль. Попробуйте снова.`
+            })
+        }
+
+    } else {
+        res.status(404).json({
+            message: `Пользователь с таким email не найден.`
+        })
+    }
 }
 
-module.exports.register = function(req,res) {
-    res.status(200).json({
-        register: "from controllers"
-    })
+module.exports.register = async function(req,res) {
+    // const user = new User({
+    //     email: req.body.email,
+    //     password: req.body.password
+    // })
+    // user.save().then(() => console.log('User created')) 
+    const candidate = await User.findOne({email: req.body.email})
+    // Конфликт
+    if(candidate){
+        res.status(409).json({   
+            message: `Такой email уже занят.`                
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try{
+            await user.save()
+            res.status(201).json(user)
+        }catch(e){
+
+        }
+       
+    }
+    
 }
